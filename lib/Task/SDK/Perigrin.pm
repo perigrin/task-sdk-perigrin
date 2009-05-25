17f6b797f341c4bb5371c79eb73b08d21b44ad22
Chris Prather <chris@prather.org>
2009-05-25 17:54:29 -0400

    add rough documentation

--- lib/Task/SDK/Perigrin.pm
+++ lib/Task/SDK/Perigrin.pm
@@ -1,132 +1,170 @@
 package Task::SDK::Perigrin;
 use strict;
-our $VERSION = '0.0.1';
-
-# Module implementation here
+our $VERSION = '0.01';
 
 1;    # Magic true value required at end of module
 __END__
 
 =head1 NAME
 
-Task::SDK::Perigrin - [One line description of module's purpose here]
+Task::SDK::Perigrin - A List of Modules I Use Constantly
 
 
 =head1 VERSION
 
-This document describes Task::SDK::Perigrin version 0.0.1
+This document describes Task::SDK::Perigrin version 0.01
 
 
 =head1 SYNOPSIS
 
-    use Task::SDK::Perigrin;
+    $> cpan Task::SDK::Perigrin
 
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
-  
-  
 =head1 DESCRIPTION
 
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
+This is a meta-package that gathers together what I find to be my standard
+install.
+
+=head1 DEPENDENCIES
 
 
-=head1 INTERFACE 
-
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
-
-
-=head1 DIAGNOSTICS
-
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
+=head2 Specific Modules
 
 =over
 
-=item C<< Error message here, perhaps with %s placeholders >>
+=item local::lib
 
-[Description of error here]
+=item Task::Kensho
 
-=item C<< Another error message here >>
+=back
 
-[Description of error here]
+=head2 Moose
 
-[Et cetera, et cetera]
+=over
+
+=item  Moose
+
+=item  namespace::autoclean
+
+=item  MooseX::GetOpt
+
+=item  MooseX::SimpleConfig
+
+=item  MooseX::AttributeHelpers
+
+=item  MooseX::POE
+
+=item  MooseX::Declare
+
+=item  MooseX::Types
+
+=item  MooseX::Types::Path::Class
+
+=item  MooseX::LogDispatch
+
+=item  MooseX::Workers
+
+=item  MooseX::MetaDescription
+
+=item  MooseX::Storage
+
+=item  Task::Moose
+
+=back
+
+=head2  Module Development
+
+=over
+
+=item  Module::Install
+
+=item  Module::Install::AutoManifest
+
+=item  Module::Install::Repository
+
+=back
+
+=head2  Bots
+
+=over
+
+=item  Task::POE::IRC
+
+=item  Regexp::Common::IRC
+
+=back
+
+=head2 Storage
+
+=over
+
+=item  KiokuDB
+
+=item  DBIx::Class
+
+=back
+
+=head2 Tools
+
+=over
+
+=item  App::Ack
+
+=item  App::Nopaste
+
+=item  Module::Corelist
+
+=back
+
+=head2 Utilities
+
+=over
+
+=item  JSON::Any
+
+=item  YAML::XS
+
+=back
+
+=head2 Web
+
+=over
+
+=item  Catalyst::Runtime
+
+=item  Catalyst::View::TT
+
+=item  Catalyst::Controller::REST
+
+=item  Catalyst::Authentication::Credential::OpenID
+
+=item  HTTP::Engine
+
+=back
+
+=head2 XML
+
+=over
+
+=item  XML::LibXML
+
+=item  XML::Toolkit
 
 =back
 
 
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
-Task::SDK::Perigrin requires no configuration files or environment variables.
-
-
-=head1 DEPENDENCIES
-
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
-
-=head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
-
-None reported.
-
-
 =head1 BUGS AND LIMITATIONS
 
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
-
-No bugs have been reported.
-
 Please report any bugs or feature requests to
-C<bug-task-sdk-perigrin@rt.cpan.org>, or through the web interface at
+C<chris@prather.org>, or through the web interface at
 L<http://rt.cpan.org>.
-
 
 =head1 AUTHOR
 
-Chris Prather  C<< <perigrin@cpan.org> >>
-
+Chris Prather  C<< <chris@prahter.org> >>
 
 =head1 LICENCE AND COPYRIGHT
 
-Copyright (c) 2007, Chris Prather C<< <perigrin@cpan.org> >>. All rights reserved.
+Copyright (c) 2007, Chris Prather C<< <chris@prahter.org> >>. Some rights reserved.
 
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself. See L<perlartistic>.
